e56ac8a35e63b7234899daa31568f865bd4e926f
geonho0116 <geonho0116@naver.com>
2020-02-05 18:41:01 +0900

    Update sum algorithm.py

--- sum algorithm.py
+++ sum algorithm.py
@@ -10,3 +10,13 @@ def sum_n(n):
 
 print(sum_n(10))
 print(sum_n(100))
+
+#1부터 n까지 연속한 숫자의 합을 구하는 알고리즘2
+#입력 : n
+#출력 : 1부터 n까지의 숫자를 더한 값
+
+def sum_n2(n):
+    return n*(n+1)/2
+
+print(sum_n2(10))
+print(sum_n2(100))
